d8b89415eb49fae11d03e22035c979508a910a9b
root <root@localhost.localdomain>
2017-12-09 00:49:32 +0100

    ra ra raspicode...

--- hardware/raspicode.py
+++ hardware/raspicode.py
@@ -1,15 +1,35 @@
 import json
 import urllib2
+import time
 from sense_hat import SenseHat
 
 sense = SenseHat()
 humidity = sense.get_humidity()
 temp = sense.get_temperature()
 
-data = json.dumps({"_id": "hanz na to cekal", "name": "mission impossible flute pole dance", lon: None, lat: None, reviews: [None], price: None, last_clean: None, last_shit: None, "type": "gateway"})
+lon = "lat"
+lat = "lon"
+reviews = "reviews"
+price = "price"
+last_clean = "last_clean"
+last_shit = "last_shit"
+try:
+	while 1:
+		data = json.dumps({"_id": "brno", "name": "brno", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
+		req.add_header('Content-Type', 'application/json')
+		response = urllib2.urlopen(req, data)
+		
+		data = json.dumps({"_id": "bruno", "name": "jupiter", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
+		req.add_header('Content-Type', 'application/json')
+		response = urllib2.urlopen(req, data)
 
-req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
-req.add_header('Content-Type', 'application/json')
-
-response = urllib2.urlopen(req, data)
+		data = json.dumps({"_id": "kubo", "name": "pls no", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
+		req.add_header('Content-Type', 'application/json')
+		response = urllib2.urlopen(req, data)		
+		time.sleep(10)
+except:
+	pass
 
