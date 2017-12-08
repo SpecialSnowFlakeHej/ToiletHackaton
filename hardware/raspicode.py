26dcba63df103bc47cf6f59b3a0da9c973d2207c
root <root@localhost.localdomain>
2017-12-08 21:57:56 +0100

    update raspicodu

--- hardware/raspicode.py
+++ hardware/raspicode.py
@@ -6,11 +6,10 @@ sense = SenseHat()
 humidity = sense.get_humidity()
 temp = sense.get_temperature()
 
-data = json.dumps({"id": 111, "inside": 6, "humidity" : humidity, "temperature" : temp})
-
+data = json.dumps({"id": 111, "inside": 6, "humidity" : humidity, "temperature"$
+#data = json.dumps({"id": 111, "inside": 6, "humidity" : 5, "temperature" : 5})
 req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
 req.add_header('Content-Type', 'application/json')
 
 response = urllib2.urlopen(req, data)
-
 
