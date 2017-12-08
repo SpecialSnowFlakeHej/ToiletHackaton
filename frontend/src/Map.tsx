dccae78b2bebf098ab2c2d199eb8491f8efcafc4
unknown <vit.pospisil@pslib.cz>
2017-12-08 23:41:30 +0100

    refactor

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Component } from "react";
 import { observer } from "mobx-react";
 import { Model } from "./Model";
+
 const {L}: any = window;
 
 export interface MapProps {
@@ -11,37 +12,52 @@ export interface MapProps {
 @observer
 export class MapComponent extends Component<MapProps, {}> {
     
-
+private map: any = null;
+private userLoc: any = null;
+private markers: any = null;
     componentDidUpdate() {
         const {model} = this.props;
-        const pos = [model.lat, model.long]
-        var mymap = L.map('map').setView(pos, 15);
+        const pos = [model.lat, model.long];
+        this.map? this.map.setView(pos, 15) :
+        this.map = L.map('map').setView(pos, 15);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
                 '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
             id: 'mapbox.streets'
-        }).addTo(mymap);
+        }).addTo(this.map);
+
+        this.markers? this.markers.clearLayers() : this.markers = new L.FeatureGroup();
         
         // device location
-        L.circle(pos, {
+        this.markers.addLayer( L.circle(pos, {
             color: 'red',
             fillColor: '#f03',
             fillOpacity: 0.8,
-            radius: 30
-        }).addTo(mymap);
+            radius: 3
+        }));
 
-        model.markers.forEach(m => L.marker([m.lat, m.long]).addTo(mymap));
+        model.data? model.data.forEach(m => {
+            this.markers.addLayer(L.marker([m.position.lat, m.position.long]).bindPopup(
+                `Jméno: ${m.name}</br>
+                Cena: ${m.price? m.price : 0} Kč</br>
+                ${m.lastCleaned? "Naposledy uklizeno: " + m.lastCleaned : ""}`));
+        }) : null;
+
+        this.map.addLayer(this.markers);
     }
 
     render() {  
         const {model} = this.props; 
         return (
             <div>
-                <div>longitude: {model.long}</div>
-                <div>lattitude: {model.lat}</div>
-                <div id="map" style={{width: "100%", height: "100vh"}} />
+                {console.log("need rerender", model.long)}
+                <input type="input" placeholder="Místo"
+                    onChange={(e) => model.onLocationChange(e)}
+                    onKeyUp={(e => {e.keyCode === 13? model.onSubmitLocation() : null})} />
+                <input type="button" onClick={model.onSubmitLocation} value="Odeslat" />
+                <div id="map" style={{width: "100%", height: "90vh"}} > </div>
             </div>
         );
     }
--- frontend/src/Model.ts
+++ frontend/src/Model.ts
@@ -1,10 +1,19 @@
 import { observable, action } from "mobx";
 import { geolocated, GeolocatedProps } from "react-geolocated";
 import Axios from "axios";
+import { Moment } from "moment";
 
 export interface IMarker {
     long: number;
     lat: number;
+}
+
+export interface IData {
+    lastCleaned: Moment;
+    lastUsed: Moment;
+    price: number;
+    position: IMarker;
+    name: string;
 }
 
 export class Model {
@@ -12,13 +21,19 @@ export class Model {
         //Axios.get(url).then(response => console.log(response.data));
         Axios.get(url).then(response => { 
             console.log("Data loaded", response.data);
-            }
-        );
+            response.data.forEach(d => this.data.push({ 
+                name: d.name,
+                price: d.price,
+                lastCleaned: d.last_clean,
+                lastUsed: d.last_shit,
+                position: {long: d.lon, lat: d.lat}
+            }))
+        });
+
     }
-    @observable data: any[];
+    @observable data: IData[] = [];
     @observable long: number = 0;
     @observable lat: number = 0;
-    @observable markers: IMarker[] = [];
 
     @action.bound getLocation() {
         if (navigator.geolocation) {
@@ -33,5 +48,20 @@ export class Model {
         this.lat = position.coords.latitude;
         console.log("Latitude: " + position.coords.latitude + 
         "Longitude: " + position.coords.longitude)
+    }
+
+    private locationText = "";
+    @action.bound onLocationChange(e: React.ChangeEvent<HTMLInputElement>){
+        this.locationText = e.target.value;
+    }
+
+    @action.bound onSubmitLocation() {
+        Axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.locationText}&key=AIzaSyA0S4pAN29Tbl0g7OeV_r_o3GtgIkU52f4`)
+        .then(response => {
+            console.log("changed pos");
+            const loc = response.data.results[0].geometry.location;
+            this.lat = loc.lat;
+            this.long = loc.lng;
+        });
     }
 }
