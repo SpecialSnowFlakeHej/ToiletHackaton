c8c35767a0022892f2c629112d1f31bda340dc8f
root <root@localhost.localdomain>
2017-12-09 01:03:46 +0100

    last_shit je pryc - RIP T_T

--- hardware/raspicode.py
+++ hardware/raspicode.py
@@ -12,20 +12,20 @@ lat = "lon"
 reviews = "reviews"
 price = "price"
 last_clean = "last_clean"
-last_shit = "last_shit"
+
 try:
 	while 1:
-		data = json.dumps({"_id": "brno", "name": "brno", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		data = json.dumps({"_id": "brno", "name": "brno", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, "type": "gateway", "temperature": temp, "humidity": humidity})
 		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
 		req.add_header('Content-Type', 'application/json')
 		response = urllib2.urlopen(req, data)
 		
-		data = json.dumps({"_id": "bruno", "name": "jupiter", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		data = json.dumps({"_id": "bruno", "name": "jupiter", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, "type": "gateway", "temperature": temp, "humidity": humidity})
 		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
 		req.add_header('Content-Type', 'application/json')
 		response = urllib2.urlopen(req, data)
 
-		data = json.dumps({"_id": "kubo", "name": "pls no", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, last_shit: None, "type": "gateway", "temperature": temp, "humidity": humidity})
+		data = json.dumps({"_id": "kubo", "name": "pls no", lon: 49.224286, lat: 16.593935, reviews: [], price: None, last_clean: None, "type": "gateway", "temperature": temp, "humidity": humidity})
 		req = urllib2.Request('https://toilethackaton.eu-gb.mybluemix.net/data')
 		req.add_header('Content-Type', 'application/json')
 		response = urllib2.urlopen(req, data)		
